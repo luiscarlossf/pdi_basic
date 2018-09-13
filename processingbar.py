e58a4e4811f544a5a0a5b82f8d44911a70d94d8f
luiscarlos <luiscarlos.sf@outlook.com>
2018-09-13 15:58:21 -0300

    add CloseButton

--- processingbar.py
+++ processingbar.py
@@ -18,6 +18,7 @@ class ProcessingBar(BoxLayout):
         self.add_widget(Label(text='Propriedades'))
         self.add_widget(self.p[index])
         self.add_widget(RevertButton(self.ui, self, text='Reverter Imagem'))
+        self.add_widget(CloseButton(self.ui, text='Fechar Imagem'))
 
     # def salveImageCurrant(self):
     #     self.image_currant = image.source
@@ -31,3 +32,15 @@ class RevertButton(Button):
 
     def on_press(self):
         self.ui.pdispace.setSourceImage(self.pb.image_currant)
+
+class CloseButton(Button):
+    def __init__(self, ui,  **kwargs):
+        self.ui =ui
+        super(CloseButton, self).__init__(**kwargs)
+
+    def on_press(self):
+        panelimages = self.ui.panelimages
+        panelimages.remove_widget(panelimages.content)
+        panelimages.remove_widget(panelimages.current_tab)
+        panelimages.switch_to(panelimages.tab_list[0],do_scroll=False)
+        self.ui.setSourceImage(" ")
