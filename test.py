b908ee48252228feb3519f2c888f7739f46a4ccd
luiscarlos <luiscarlos.sf@outlook.com>
2018-09-13 09:11:29 -0300

    add new test class

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-import cv2 as cv
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.image import Image as CoreImage
+from kivy.core.image import ImageData
+
+class Test(BoxLayout):
+    pass
+
+class TestApp(App):
+    def build(self):
+        t = Test()
+        im = CoreImage("./images/estrada.jpg")
+        imd = ImageData()
+        t.add_widget(im.image)
+        return Test()
 
 if __name__=="__main__":
-    img = cv.imread("./vieira2.png")
-    for i in img:
-        print(i)
+    TestApp().run()
 
