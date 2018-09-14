a80fcedd694775a23cf36fac74224711e42b69db
luiscarlos <luiscarlos.sf@outlook.com>
2018-09-14 03:53:09 -0300

    Fixed CloseButton and update RevertButton

--- processingbar.py
+++ processingbar.py
@@ -5,20 +5,21 @@ from kivy.uix.button import Button
 from properties import DefaultProperties,TransformProperties, BitProperties, HistogramProperties
 from properties import DetectionProperties, FatColorProperties, FilterProperties
 import ip
+import os
 
 class ProcessingBar(BoxLayout):
     ui = ObjectProperty(None)
     image_currant = str()
-    p = [DefaultProperties(), TransformProperties(ui=ui), BitProperties(),HistogramProperties(), \
+    p = [DefaultProperties(), TransformProperties(ui=ui), BitProperties(),HistogramProperties(ui=ui), \
          FilterProperties(), DetectionProperties(), FatColorProperties()]
 
     def addProperties(self, index):
         self.clear_widgets()
         self.p[index].ui = self.ui
-        self.add_widget(Label(text='Propriedades'))
+        self.add_widget(Label(text='Propriedades', size_hint=(1, 0.2)))
         self.add_widget(self.p[index])
-        self.add_widget(RevertButton(self.ui, self, text='Reverter Imagem'))
-        self.add_widget(CloseButton(self.ui, text='Fechar Imagem'))
+        self.add_widget(RevertButton(self.ui, self, text='Reverter', size_hint=(1, 0.3)))
+        self.add_widget(CloseButton(self.ui, text='Fechar Imagem', size_hint=(1, 0.3)))
 
     # def salveImageCurrant(self):
     #     self.image_currant = image.source
@@ -31,6 +32,7 @@ class RevertButton(Button):
         super(RevertButton, self).__init__(**kwargs)
 
     def on_press(self):
+        os.remove(self.ui.getSourceImage())
         self.ui.pdispace.setSourceImage(self.pb.image_currant)
 
 class CloseButton(Button):
@@ -39,7 +41,7 @@ class CloseButton(Button):
         super(CloseButton, self).__init__(**kwargs)
 
     def on_press(self):
-        panelimages = self.ui.panelimages
+        panelimages = self.ui.pdispace.panelimages
         panelimages.remove_widget(panelimages.content)
         panelimages.remove_widget(panelimages.current_tab)
         panelimages.switch_to(panelimages.tab_list[0],do_scroll=False)
