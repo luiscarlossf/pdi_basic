f68a16dd99067f254454a083bdfff7f2dee376da
luiscarlos <luiscarlos.sf@outlook.com>
2018-09-14 03:52:03 -0300

    Add news properties

--- properties.py
+++ properties.py
@@ -2,10 +2,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.dropdown import DropDown
 
+from image_pdi import Image as PI
+
+from backend_kivyagg import FigureCanvasKivyAgg
+from kivy.uix.button import Button
+
 from os import path
 from kivy.lang.builder import Builder
 import cv2 as cv
 import ip
+
+from image_pdi import Image as PI
 
 
 
@@ -30,7 +37,11 @@ class TransformProperties(BoxLayout):
             self.source = img.source
         else:
             self.first +=1
-        ip.power_transform(img.source, int(self.consttextinput.text), float(self.gamatextinput.text))
+        try:
+            offset = float(self.offsettextinput.text)
+            ip.power_transform(img.source, int(self.consttextinput.text), float(self.gamatextinput.text), offset)
+        except ValueError:
+            ip.power_transform(img.source, int(self.consttextinput.text), float(self.gamatextinput.text))
         img.source = "./images/temporarias/"+path.basename(img.source)
         img.reload()
 
@@ -47,18 +58,56 @@ class HistogramProperties(BoxLayout):
         self.ui = ui
         super(HistogramProperties, self).__init__(**kwargs)
 
+
+    def get_gcf(self, source):
+        plt = PI(filename=source).histogram()
+        return plt
+
     def equalizar(self):
-        pass
+        self.hist = FigureCanvasKivyAgg(self.get_gcf("./images/deadpool.jpg"))
+        self.add_widget(self.hist)
+        source = self.ui.getSourceImage()
+        plt = PI(filename=source).equalize()
+        self.hist.figure = plt
 
 class FilterProperties(BoxLayout):
+    kernelTextInput = ObjectProperty(None)
+    mediana = ObjectProperty(None)
+    media = ObjectProperty(None)
+    max = ObjectProperty(None)
+    min = ObjectProperty(None)
+    first = 1
     def __init__(self, ui=None, **kwargs):
         self.ui = ui
         super(FilterProperties, self).__init__(**kwargs)
+
+    def on_press(self):
+        source = self.ui.getSourceImage()
+        self.ui.processingbar.image_currant = source
+        if self.mediana.active:
+            print("Mediana")
+            new = PI(source).median_filter(int(self.kernelTextInput.text))
+            self.ui.setSourceImage(new)
+        elif self.media.active:
+            print("Media")
+            new = PI(source).media_filter(int(self.kernelTextInput.text))
+            self.ui.setSourceImage(new)
+        elif self.max.active:
+            pass
+        elif self.min.active:
+            pass
+
 
 class DetectionProperties(BoxLayout):
     def __init__(self, ui=None, **kwargs):
         self.ui = ui
         super(DetectionProperties, self).__init__(**kwargs)
+
+    def detectar(self):
+        source = self.ui.getSourceImage()
+        self.ui.processingbar.image_currant = source
+        new = PI(source).contours_canny()
+        self.ui.setSourceImage(new)
 
 class FatColorProperties(BoxLayout):
     def __init__(self, ui=None, **kwargs):
