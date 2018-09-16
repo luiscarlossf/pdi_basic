40d1348532f0c8d477d50a2158eebfee89ed6baf
luiscarlos <luiscarlos.sf@outlook.com>
2018-09-16 18:31:16 -0300

    Update histogram function and add new properties such as BitProperties

--- properties.py
+++ properties.py
@@ -21,7 +21,6 @@ class TransformProperties(BoxLayout):
     gamatextinput = ObjectProperty(None)
     offsettextinput = ObjectProperty(None)
     transformbutton = ObjectProperty(None)
-    first = 1
     source = None
 
     def __init__(self, ui=None, **kwargs):
@@ -30,33 +29,40 @@ class TransformProperties(BoxLayout):
         super(TransformProperties, self).__init__(**kwargs)
 
     def on_press(self):
-        #item=self.panelimages.current_tab
         img = self.ui.pdispace.getImage()
-        if (self.first == 1) or not(self.source == img.source):
-            self.ui.processingbar.image_currant = img.source
-            self.source = img.source
-        else:
-            self.first +=1
+        self.ui.processingbar.image_currant = str(img.source)
+        ip = PI(filename=self.ui.getSourceImage())
         try:
             offset = float(self.offsettextinput.text)
-            ip.power_transform(img.source, int(self.consttextinput.text), float(self.gamatextinput.text), offset)
+            newfilename = ip.power(int(self.consttextinput.text), float(self.gamatextinput.text), offset)
         except ValueError:
-            ip.power_transform(img.source, int(self.consttextinput.text), float(self.gamatextinput.text))
-        img.source = "./images/temporarias/"+path.basename(img.source)
+            newfilename = ip.power(int(self.consttextinput.text), float(self.gamatextinput.text))
+        img.source = newfilename
         img.reload()
 
 class BitProperties(BoxLayout):
     def __init__(self, ui=None, **kwargs):
         self.ui = ui
+        self.lista = list()
         super(BitProperties, self).__init__(**kwargs)
+
+    def on_press(self):
+        pass
 
 class HistogramProperties(BoxLayout):
     hist = ObjectProperty(None)
     button = ObjectProperty(None)
+    filename = " "
 
     def __init__(self, ui=None, **kwargs):
         self.ui = ui
         super(HistogramProperties, self).__init__(**kwargs)
+
+
+    def setHistogram(self):
+        newfilename = PI(filename=self.ui.pdispace.getImage().source).histogram()
+        self.hist.source = newfilename
+        self.hist.reload()
 
 
     def get_gcf(self, source):
@@ -64,11 +70,14 @@ class HistogramProperties(BoxLayout):
         return plt
 
     def equalizar(self):
-        self.hist = FigureCanvasKivyAgg(self.get_gcf("./images/deadpool.jpg"))
-        self.add_widget(self.hist)
-        source = self.ui.getSourceImage()
-        plt = PI(filename=source).equalize()
-        self.hist.figure = plt
+        img = self.ui.pdispace.getImage()
+        source = str(img.source)
+        self.ui.processingbar.image_currant = source
+        new = PI(filename=source).equalize()
+        img.source = new
+        img.reload()
+        self.hist.reload()
+
 
 class FilterProperties(BoxLayout):
     kernelTextInput = ObjectProperty(None)
@@ -82,20 +91,24 @@ class FilterProperties(BoxLayout):
         super(FilterProperties, self).__init__(**kwargs)
 
     def on_press(self):
-        source = self.ui.getSourceImage()
+
+        img = self.ui.pdispace.getImage()
+        source = str(img.source)
         self.ui.processingbar.image_currant = source
         if self.mediana.active:
             print("Mediana")
             new = PI(source).median_filter(int(self.kernelTextInput.text))
-            self.ui.setSourceImage(new)
         elif self.media.active:
             print("Media")
             new = PI(source).media_filter(int(self.kernelTextInput.text))
-            self.ui.setSourceImage(new)
         elif self.max.active:
-            pass
+            print("Max")
+            new = PI(source).media_filter(int(self.kernelTextInput.text))
         elif self.min.active:
-            pass
+            print("Min")
+            new = PI(source).media_filter(int(self.kernelTextInput.text))
+        img.source = new
+        img.reload()
 
 
 class DetectionProperties(BoxLayout):
@@ -104,10 +117,12 @@ class DetectionProperties(BoxLayout):
         super(DetectionProperties, self).__init__(**kwargs)
 
     def detectar(self):
-        source = self.ui.getSourceImage()
+        img = self.ui.pdispace.getImage()
+        source = str(img.source)
         self.ui.processingbar.image_currant = source
         new = PI(source).contours_canny()
-        self.ui.setSourceImage(new)
+        img.source = new
+        img.reload()
 
 class FatColorProperties(BoxLayout):
     def __init__(self, ui=None, **kwargs):
